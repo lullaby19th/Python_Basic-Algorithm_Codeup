52858fd2a641600f4c1bbcaf8a755fcc69b11713
Minjun NO <58175593+lullaby19th@users.noreply.github.com>
2019-12-04 00:14:47 +0900

    Add files via upload

---
+++ 1013_[기초-입출력] 정수 2개 입력받아 그대로 출력하기(설명).py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 00:11:15 2019
+
+@author: 노민준
+"""
+
+a,b = input().split()
+
+#input에 split을 사용하면 입력받은 값을 
+#공백을 기준으로 분리하여 변수에 차례대로 저장한다.
+print(type(a)) #str 문자열
+print(type(b)) #str 문자열
+
+x=int(a)
+
+y=int(b)
+
+print(x, y)
