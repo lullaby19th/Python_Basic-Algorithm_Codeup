c29cf6ae60043c645f36a30e3de0dfd694015a5b
Minjun NO <58175593+lullaby19th@users.noreply.github.com>
2019-12-05 01:37:00 +0900

    Add files via upload

---
+++ 1024_[기초-입출력] 단어 1개 입력받아 나누어 출력하기(설명).py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  5 01:28:53 2019
+
+@author: 노민준
+"""
+
+a = input()
+
+
+for i in range(len(a)):
+    print("'%s'" %a[i]) # 문자 '를 표현하려면 양쪽에 "를 붙여줘야함.
